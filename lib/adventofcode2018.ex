2aa99388e513aae669096b78524d3c0c395d2ffc
Arjan Molenaar <gaphor@gmail.com>
2018-11-30 14:56:31 +0100

    Add Elixir skeleton

---
+++ lib/adventofcode2018.ex
@@ -0,0 +1,18 @@
+defmodule Adventofcode2018 do
+  @moduledoc """
+  Documentation for Adventofcode2018.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventofcode2018.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/adventofcode2018_test.exs
@@ -0,0 +1,8 @@
+defmodule Adventofcode2018Test do
+  use ExUnit.Case
+  doctest Adventofcode2018
+
+  test "greets the world" do
+    assert Adventofcode2018.hello() == :world
+  end
+end
